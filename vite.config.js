1be7bbefb4f1c8cb69d659d0e0cb8502796674b6
Thomas <tomstur96flightsim@gmail.com>
2023-10-28 15:11:44 +1100

    setup run build for Vite with multiple pages/sources added source folder added public folder

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import htmlPurge from 'vite-plugin-html-purgecss'
+import htmlPurge from "vite-plugin-html-purgecss";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+// export default {
+//   plugins: [htmlPurge()],
+// };
 
-export default {
-    plugins: [
-        htmlPurge(),
-    ]
-}
+// import { resolve } from "path";
+// import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: resolve(__dirname, "src/"),
+  plugins: [htmlPurge()],
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        home: resolve(__dirname, "src/index.html"),
+        // about: resolve(__dirname, "src/about/index.html"),
+        // contact: resolve(__dirname, "src/contact/index.html"),
+      },
+    },
+  },
+});
